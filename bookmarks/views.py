a6bd4a844a1a37287f168042df5f86b36bcbd5fb
Pyunghyuk Yoo <yoophi@gmail.com>
2013-11-08 16:31:57 +0900

    ch4.3

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -4,15 +4,14 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
-from django.template import Context
+from django.template import RequestContext
 from django.template.loader import get_template
 from django.shortcuts import render_to_response
 
 
 def main_page(request):
     return render_to_response(
-        'main_page.html',
-        {'user': request.user}
+        'main_page.html', RequestContext(request)
     )
 
 
@@ -25,7 +24,7 @@ def user_page(request, username):
     bookmarks = user.bookmark_set.all()
 
     template = get_template('user_page.html')
-    variables = Context({
+    variables = RequestContext(request, {
         'username': username,
         'bookmarks': bookmarks
     })
